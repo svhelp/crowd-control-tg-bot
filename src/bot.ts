b01e73d0c4d89740e669a800e308a56e76c50159
Aleksandr Kravchenko <svhelpp@gmail.com>
2024-01-06 19:20:27 +0100

    - introduced index file;

---
+++ src/bot.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+import TelegramBot from 'node-telegram-bot-api'
+
+const token = process.env.TELEGRAM_API_KEY;
+
+export const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const message = msg.text;
+
+  bot.sendMessage(chatId, `Received your message: ${message}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import 'dotenv/config'
-import TelegramBot from 'node-telegram-bot-api'
-
-const token = process.env.TELEGRAM_API_KEY;
-
-const bot = new TelegramBot(token, { polling: true });
-
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  const message = msg.text;
-
-  bot.sendMessage(chatId, `Received your message: ${message}`);
-});
+export { bot } from './bot';
