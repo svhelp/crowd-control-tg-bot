437e51c4310836948af1ffa1428bfd82cd620363
Aleksandr Kravchenko <svhelpp@gmail.com>
2024-01-13 19:08:22 +0100

    - changed tg library;

--- api/server.ts
+++ api/server.ts
@@ -1,16 +1,13 @@
 import { bot } from '../src/bot';
 
-export default async function handleRequest(request: Request) {
-  if (!request.json) {
-    console.error("Wrong request: " + JSON.stringify(request));
-    return new Response("Something went wrong", { status: 500 });
-  }
+const webhookDomain = process.env.WEBHOOK_DOMAIN ?? '';
+const webhookPath = process.env.WEBHOOK_PATH ?? '';
 
-  try {
-      bot.processUpdate(await request.json());
-      return new Response("", { status: 200 });
-  } catch (e) {
-      console.error(e);
-      return new Response("Something went wrong", { status: 500 });
-  }
+if (!webhookDomain) {
+  throw new Error("Initialization error. Webhook domain config is missing.");
 }
+
+export default bot.createWebhook({
+  domain: webhookDomain,
+  path: webhookPath,
+});
--- src/bot.ts
+++ src/bot.ts
@@ -1,22 +1,11 @@
 import 'dotenv/config'
-import TelegramBot from 'node-telegram-bot-api'
+import { message } from "telegraf/filters"
+import { Telegraf } from "telegraf";
 
 const token = process.env.TELEGRAM_API_KEY ?? '';
-const webhookAddress = process.env.WEBHOOK_ADDRESS ?? '';
 
-const botOptions: TelegramBot.ConstructorOptions = webhookAddress
-  ? undefined
-  : { polling: true }
+export const bot = new Telegraf(token);
 
-export const bot = new TelegramBot(token, botOptions);
-
-if (webhookAddress) {
-  bot.setWebHook(webhookAddress)
-}
-
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  const message = msg.text;
-
-  bot.sendMessage(chatId, `Received your message: ${message}`);
+bot.on(message('text'), ctx => {
+  ctx.sendMessage(`I've got your message: "${ctx.update.message.text}". Please send me another one!`)
 });
